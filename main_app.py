834bd44566704094fd6de4c39b897dc303b794d4
galben23990 <153602946+galben23990@users.noreply.github.com>
2024-05-27 16:08:09 +0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,19 +1,19 @@
 import streamlit as st
 from pages import Calculate_Holdings
-from pages import Create_Investor_PNL
+from pages import Calculate_Matrix
 import time
 from datetime import date
 
 def main():
     st.set_page_config(layout="wide")
     # Create tabs
-    tab1,tab2 = st.tabs(["Calculate Holdings", "Create Investor PNL"])
+    tab1,tab2 = st.tabs(["Calculate Holdings", "Calculate Matrix"])
 
     # Define the content of each tab
     with tab1:
         Calculate_Holdings.Calculate_Holdings()
     with tab2:
-        Create_Investor_PNL.Create_Investor_PNL()
+        Calculate_Matrix.Calculate_Matrix()
 
 
 
